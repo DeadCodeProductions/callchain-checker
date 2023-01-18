36e0b266f22a9ad16390995f2a84b111f658a795
Theodoros Theodoridis <theodoros.theodoridis@inf.ethz.ch>
2023-01-18 18:09:54 +0100

    Fix CI

--- build_python_wheel_local.sh
+++ build_python_wheel_local.sh
@@ -8,7 +8,8 @@ cd build
 cmake -G Ninja .. -DCMAKE_BUILD_TYPE=Release \
                   -DCMAKE_CXX_FLAGS="-static-libgcc -static-libstdc++ -fPIC" \
                   -DCMAKE_EXE_LINKER_FLAGS="-static-libgcc -static-libstdc++" \
-                  -DCMAKE_SHARED_LINKER_FLAGS="-static-libgcc -static-libstdc++"
+                  -DCMAKE_SHARED_LINKER_FLAGS="-static-libgcc -static-libstdc++" \
+                  -DCMAKE_MODULE_PATH="$CMAKE_MODULE_PATH"
 ninja 
 cd ..
 cp build/bin/ccc python_src/callchain_checker
